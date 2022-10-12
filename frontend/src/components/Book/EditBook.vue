3bc367b9dd468eb93ab7fe4124c168d11cc43526
octopus84 <octaviogodoyg@gmail.com>
2022-10-12 03:01:23 -0300

    Vista para editar

---
+++ frontend/src/components/Book/EditBook.vue
@@ -0,0 +1,59 @@
+<template lang="html">
+    <div class="container">
+        <div class="row">
+            <div class="col text-left">
+                <h2>
+                    Editar Libro
+                </h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <form @onSubmit="onSubmit">
+                            <div class="form-group row">
+                                <label for="title" class="col-sm-2 col-form-label">Título</label>
+                                <div class="col-sm-6">
+                                    <input type="text" placeholder="Título" name="title" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="description" class="col-sm-2 col-form-label">Descripción</label>
+                                <div class="col-sm-6">
+                                    <textarea placeholder="Descripción" name="description" class="form-control"
+                                        cols="30" rows="10">
+
+                                    </textarea>
+                                </div>
+                            </div>
+                            <div class="rows">
+                                <div class="col text-left">
+                                    <span>
+                                        <b-btn type="submit" size="lg" variant="primary">Editar</b-btn>
+                                    </span>
+                                    <span>
+                                        <b-btn type="submit" size="lg" variant="danger" :to="{ name: 'ListBook'} ">Cancelar</b-btn>
+                                    </span>
+
+                                </div>
+
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="css" scoped>
+
+</style>
--- frontend/src/components/Book/ListBook.vue
+++ frontend/src/components/Book/ListBook.vue
@@ -5,7 +5,14 @@
                 <h2>Listado de Libros</h2>
                 <div class="col-md-12">
                     <b-table striped hover :items="books" :fields="fields">
-
+                        <template v-slot:cell(action)="{ item }">
+                            <span>
+                                <b-btn @click="editItem(item)" size="lg" variant="primary">Editar</b-btn>
+                            </span>
+                            <span>
+                                <b-btn @click="editItem(item)" size="lg" variant="danger">Eliminar</b-btn>
+                            </span>
+                        </template>
                     </b-table>
                 </div>
             </div>
@@ -16,34 +23,34 @@
 <script>
 import axios from 'axios';
 
-    export default{
-        data() {
-            return {
-                fields: [
-                    {key: 'title', label: 'Titulo'},
-                    {key: 'decription', label: 'Decripcion'},
-                    {key: 'action', label: 'Acción'},
-                ],
-                books:[]
-            }
-        },
-        methods: {
-            getBooks(){
-                const path = 'http://127.0.0.1:8000/api/v1.0/books/'
-                axios.get(path).then((response)=>{
-                    this.books = response.data
-                })
+export default {
+    data() {
+        return {
+            fields: [
+                { key: 'title', label: 'Titulo' },
+                { key: 'description', label: 'Decripcion' },
+                { key: 'action', label: 'Actión' },
+            ],
+            books: []
+        }
+    },
+    methods: {
+        getBooks() {
+            const path = 'http://127.0.0.1:8000/api/v1.0/books/'
+            axios.get(path).then((response) => {
+                this.books = response.data
+            })
                 .catch((error) => {
                     console.log(error)
                 })
-            }
-        },
-        created() {
-            this.getBooks()
-        },
-    }
+        }
+    },
+    created() {
+        this.getBooks()
+    },
+}
 </script>
 
 <style lang="css" scoped>
-    
+
 </style>
